97fa377ee91a67d48a6fad861b9528f7a1a95125
nxtnilesh <nxtnilesh@gmail.com>
2025-02-06 10:50:01 +0530

    add: backend setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connect from "./src/db/index.js";
+import logger from "./src/utils/logger.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+connect()
+  .then(
+    app.listen(PORT, () => {
+      logger.info(`Server is running on ${PORT}`);
+    })
+  )
+  .catch((err) => logger.error("MongoDB connect error", err));
+
